8e0239f207fc01020100587e3596d447eface25e
MirelaHoly <mate.pipunic@gmail.com>
2014-11-06 23:36:22 +0100

    Ispravljen nedostatak komentara

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+#Program pozdravlja svijet
 print "Hello World!"
+
+#Program shvaca da je zaboravio nesto nadodati
 print "Jos nesto"
+
+#Program eksplicitno naglasava da mu je ovo zadnja promjena
 print "zadnja promjena"
